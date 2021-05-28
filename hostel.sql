752bfc82e7d33ac39db06f0b19297e0e0ae00eb0
Viktor <kushichka@gmail.com>
2021-05-29 00:25:26 +0200

    db updated

--- hostel.sql
+++ hostel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Май 25 2021 г., 20:49
+-- Время создания: Май 29 2021 г., 00:24
 -- Версия сервера: 10.4.19-MariaDB
 -- Версия PHP: 8.0.6
 
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `roomID` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `username`, `roomID`) VALUES
+(4, 'kushichka@gmail.com', '17'),
+(8, 'kushichka@gmail.com', '4');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `room`
 --
 
@@ -31,20 +51,33 @@ CREATE TABLE `room` (
   `id` int(11) NOT NULL,
   `city` varchar(32) NOT NULL,
   `type` int(1) NOT NULL,
-  `status` varchar(32) NOT NULL
+  `status` varchar(32) NOT NULL,
+  `imgID` int(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `room`
 --
 
-INSERT INTO `room` (`id`, `city`, `type`, `status`) VALUES
-(1, 'Gdansk', 1, '1'),
-(2, 'Gdansk', 1, '1'),
-(3, 'Gdansk', 2, '0'),
-(4, 'Gdansk', 2, '1'),
-(5, 'Gdansk', 3, '0'),
-(6, 'Gdansk', 3, '1');
+INSERT INTO `room` (`id`, `city`, `type`, `status`, `imgID`) VALUES
+(1, 'Gdansk', 1, '1', 1),
+(2, 'Gdansk', 1, '1', 2),
+(3, 'Gdansk', 2, '0', 3),
+(4, 'Gdansk', 2, '1', 4),
+(5, 'Gdansk', 3, '0', 5),
+(6, 'Gdansk', 3, '1', 6),
+(7, 'London', 1, '1', 1),
+(8, 'London', 1, '1', 2),
+(9, 'London', 2, '0', 3),
+(10, 'London', 2, '1', 4),
+(11, 'London', 3, '1', 5),
+(12, 'London', 3, '0', 6),
+(13, 'Paris', 1, '0', 1),
+(14, 'Paris', 1, '0', 2),
+(15, 'Paris', 2, '1', 3),
+(16, 'Paris', 2, '0', 4),
+(17, 'Paris', 3, '1', 5),
+(18, 'Paris', 3, '0', 6);
 
 -- --------------------------------------------------------
 
@@ -73,6 +106,12 @@ INSERT INTO `user` (`id`, `firstName`, `secondName`, `email`, `password`) VALUES
 --
 
 --
+-- Индексы таблицы `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `room`
 --
 ALTER TABLE `room`
@@ -90,16 +129,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT для таблицы `room`
 --
 ALTER TABLE `room`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
